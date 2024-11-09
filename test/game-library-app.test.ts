0aed5dadbc8185680bd35a2f92fcbbc7b080bc5f
WhatIsRove <whatisbuff@gmail.com>
2024-11-09 20:42:10 +0000

    Initial commit

---
+++ test/game-library-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GameLibraryApp from '../lib/game-library-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/game-library-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GameLibraryApp.GameLibraryAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
