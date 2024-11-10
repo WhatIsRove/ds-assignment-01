9407c555c2baec3541d5deddf9c5cbeecb7ad217
WhatIsRove <whatisbuff@gmail.com>
2024-11-10 02:24:25 +0000

    Seeded games, setup rest API

--- lib/game-library-app-stack.ts
+++ lib/game-library-app-stack.ts
@@ -1,6 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
+import * as custom from "aws-cdk-lib/custom-resources";
+import { generateBatch } from '../shared/util';
+import { games } from '../seed/games';
+import * as apig from "aws-cdk-lib/aws-apigateway";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class GameLibraryAppStack extends cdk.Stack {
@@ -14,6 +18,8 @@ export class GameLibraryAppStack extends cdk.Stack {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
 
+
+    //create dynamodb table for games
     const gamesTable = new dynamodb.Table(this, "gamesTable", {
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
       partitionKey: { name: "id", type: dynamodb.AttributeType.NUMBER },
@@ -22,6 +28,42 @@ export class GameLibraryAppStack extends cdk.Stack {
       tableName: "Games",
     });
 
+    //seed function for batch of games
+    new custom.AwsCustomResource(this, "gamesDatabaseInitData", {
+      onCreate: {
+        service: "DynamoDB",
+        action: "batchWriteItem",
+        parameters: {
+          RequestItems: {
+            [gamesTable.tableName]: generateBatch(games),
+          },
+        },
+        physicalResourceId: custom.PhysicalResourceId.of("gamesDatabaseInitData"), //.of(Date.now().toString()),
+      },
+      policy: custom.AwsCustomResourcePolicy.fromSdkCalls({
+        resources: [gamesTable.tableArn],
+      }),
+    });
 
+
+    //rest api
+    const api = new apig.RestApi(this, "RestAPI", {
+      description: "a game library api",
+      deployOptions: {
+        stageName: "home"
+      },
+      defaultCorsPreflightOptions: {
+        allowHeaders: ["Content-Type", "X-Amz-Date"],
+        allowMethods: ["OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE"],
+        allowCredentials: true,
+        allowOrigins: ["*"]
+      }
+    });
+
+    const gamesEndpoint = api.root.addResource("games");
+    gamesEndpoint.addMethod(
+      "GET",
+      //new apig.LambdaIntegration(..., { proxy: true })
+    );
   }
 }
